8110ea1114c75929810d8a4dfa2c0b38a8086acb
Paul <pacalabre@gmail.com>
2023-03-14 18:25:00 -0600

    Added storybook, rxjs/store, and routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './route-components/home/home.component';
+import { LoginComponent } from './route-components/login/login.component';
+import { NotFoundComponent } from './route-components/not-found/not-found.component';
+import { ProfileComponent } from './route-components/profile/profile.component';
+import { RegisterComponent } from './route-components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './route-components/home/home.component';
+import { ProfileComponent } from './route-components/profile/profile.component';
+import { LoginComponent } from './route-components/login/login.component';
+import { NotFoundComponent } from './route-components/not-found/not-found.component';
+import { RegisterComponent } from './route-components/register/register.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoginComponent,
+    NotFoundComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
