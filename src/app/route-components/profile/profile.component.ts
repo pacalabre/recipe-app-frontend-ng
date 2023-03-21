a6fe3813f622b7c03269c0544bb85b96cf24ba79
Paul <pacalabre@gmail.com>
2023-03-21 13:46:01 -0600

    Populated state from the store to the profile view.

--- src/app/route-components/profile/profile.component.ts
+++ src/app/route-components/profile/profile.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
-
+  user: any = null;
+  constructor(private readonly store: Store) {
+    this.store.subscribe((state: any) => {
+      const store = state;
+      this.user = store.appState.user;
+    });
+  }
 }
